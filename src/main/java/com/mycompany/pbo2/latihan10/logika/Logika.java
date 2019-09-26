6629ac35b2df13d354ac6eff87afe160cdc47083
StevenDanesswaralay <55594713+StevenDanesswaralay@users.noreply.github.com>
2019-09-26 11:22:00 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/pbo2/latihan10/logika/Logika.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pbo2.latihan10.logika;
+
+/**
+ *
+ * @author 
+ * NAMA     : Steven Danesswaralay
+ * KELAS    : PBO2
+ * NIM      : 10118078
+ * Deskripsi Program : Logika
+ */
+public class Logika {
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+}
